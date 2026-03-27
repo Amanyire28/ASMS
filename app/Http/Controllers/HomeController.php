0750fcd381caf6c20b1ce8627c99d98159e38099
Amanyire28 <fedelcastral28@gmail.com>
2026-03-27 14:29:02 +0300

    Restore missing Cache facade import in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Models\Announcement;
 use App\Models\Mark;
 use App\Models\ReportGeneration;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use App\Models\Student;
 use App\Models\Teacher;
 use App\Models\ClassModel;
